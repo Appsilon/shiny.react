ca79e3f69ec9f87249d38ee64d816a176d6754d9
Kamil Zyla <kamil@appsilon.com>
2022-09-30 10:46:08 +0200

    feat: Add ReactContext test component

--- js/src/test-components/index.js
+++ js/src/test-components/index.js
@@ -1,3 +1,4 @@
 export { default as Box } from './Box';
 export { default as Counter } from './Counter';
 export { default as Pinger } from './Pinger';
+export { default as ReactContext } from './ReactContext';
