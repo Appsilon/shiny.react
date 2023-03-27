a7490c40aaca7ca258a5ca42e56c69ccdfcd5a66
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2023-03-27 11:25:34 +0200

    fix: disable import/no-unresolved linter for html-react-parser

--- js/src/react/mapReactData.js
+++ js/src/react/mapReactData.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line import/no-unresolved
 import parse from 'html-react-parser';
 
 import { needsBindingWrapper, ShinyBindingWrapper } from './shinyBindings';
