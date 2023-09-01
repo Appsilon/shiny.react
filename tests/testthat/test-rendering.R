e6083bac7fadb2208a9d77ca5e0116942c9f8710
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2023-09-01 09:32:52 +0200

    feat: add tests for rendering html in react

---
+++ tests/testthat/test-rendering.R
@@ -0,0 +1,57 @@
+init_driver <- function(app) {
+  shinytest2::AppDriver$new(app, variant = shinytest2::platform_variant())
+}
+
+describe("rendering with htmltools::HTML", {
+  it("renders HTML strings in React context from ui function when wrapped with `htmltools::HTML`", {
+    skip_on_cran()
+
+    app <- init_driver(shinyApp(
+      ui = ReactContext(HTML(
+        "<span style='font-weight: bold;'>Hello <span style='font-weight: normal;'>from ReactContext in UI</span></span>"
+      )),
+      server = function(input, output) {}
+    ))
+
+    app$expect_screenshot(
+      name = "render_html_from_ui",
+      cran = FALSE
+    )
+  })
+
+  it("renders HTML strings from renderReact when wrapped with `htmltools::HTML`", {
+    skip_on_cran()
+
+    app <- init_driver(shinyApp(
+      ui = reactOutput("react_output"),
+      server = function(input, output) {
+        output$react_output <- renderReact({
+          HTML(
+            "<span style='font-weight: bold;'>Hello <span style='font-weight: normal;'>from ReactContext in renderReact</span></span>"
+          )
+        })
+      }
+    ))
+
+    app$expect_screenshot(
+      name = "render_html_from_server",
+      cran = FALSE
+    )
+  })
+
+  it("doesn't render HTML strings in React context without `htmltools::HTML`", {
+    skip_on_cran()
+
+    app <- init_driver(shinyApp(
+      ui = ReactContext(
+        "<span style='font-weight: bold;'>Hello <span style='font-weight: normal;'>from ReactContext in UI without htmltools::HTML</span></span>"
+      ),
+      server = function(input, output) {}
+    ))
+
+    app$expect_screenshot(
+      name = "doesnt_render_html_from_ui",
+      cran = FALSE
+    )
+  })
+})
