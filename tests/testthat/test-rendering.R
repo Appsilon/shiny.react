9a83fd211199506adf1893df33b58edc5780b1a3
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2023-09-01 12:28:38 +0200

    refactor: switch to testing html instead of screenshots

--- tests/testthat/test-rendering.R
+++ tests/testthat/test-rendering.R
@@ -1,4 +1,4 @@
-init_driver <- function(app) {
+initDriver <- function(app) {
   shinytest2::AppDriver$new(app, variant = shinytest2::platform_variant())
 }
 
@@ -6,52 +6,74 @@ describe("rendering with htmltools::HTML", {
   it("renders HTML strings in React context from ui function when wrapped with `htmltools::HTML`", {
     skip_on_cran()
 
-    app <- init_driver(shiny::shinyApp(
+    # Arrange
+    app <- initDriver(shiny::shinyApp(
       ui = ReactContext(htmltools::HTML(
-        "<span style='font-weight: bold;'>Hello <span style='font-weight: normal;'>from ReactContext in UI</span></span>"
+        '<span id="test">Hello<span style="font-weight: bold;"> World</span></span>'
       )),
       server = function(input, output) {}
     ))
+    withr::defer(app$stop())
 
-    app$expect_screenshot(
-      name = "render_html_from_ui",
-      cran = FALSE
+    # Act
+    html <- app$get_html("#test")
+
+    # Assert
+    expect_equal(
+      html,
+      '<span id="test">Hello<span style="font-weight: bold;"> World</span></span>'
     )
   })
 
   it("renders HTML strings from renderReact when wrapped with `htmltools::HTML`", {
     skip_on_cran()
 
-    app <- init_driver(shiny::shinyApp(
+    # Arrange
+    app <- initDriver(shiny::shinyApp(
       ui = reactOutput("react_output"),
       server = function(input, output) {
         output$react_output <- renderReact({
           htmltools::HTML(
-            "<span style='font-weight: bold;'>Hello <span style='font-weight: normal;'>from ReactContext in renderReact</span></span>"
+            '<span id="test">Hello<span style="font-weight: bold;"> World</span></span>'
           )
         })
       }
     ))
+    withr::defer(app$stop())
 
-    app$expect_screenshot(
-      name = "render_html_from_server",
-      cran = FALSE
+    # Act
+    html <- app$get_html("#test")
+
+    # Assert
+    expect_equal(
+      html,
+      '<span id="test">Hello<span style="font-weight: bold;"> World</span></span>'
     )
   })
 
   it("doesn't render HTML strings in React context without `htmltools::HTML`", {
     skip_on_cran()
 
-    app <- init_driver(shiny::shinyApp(
+    # Arrange
+    app <- initDriver(shiny::shinyApp(
       ui = ReactContext(
-        "<span style='font-weight: bold;'>Hello <span style='font-weight: normal;'>from ReactContext in UI without htmltools::HTML</span></span>"
+        '<span id="test">Hello<span style="font-weight: bold;"> World</span></span>'
       ),
       server = function(input, output) {}
     ))
+    withr::defer(app$stop())
 
-    app$expect_screenshot(
-      name = "doesnt_render_html_from_ui",
-      cran = FALSE
+    # Act
+    html <- app$get_html("#test")
+    htmlContainer <- app$get_html(".react-container")
+
+    # Assert
+    # Span hasn't been rendered as HTML, so it's null
+    expect_null(html)
+    # The container div has been rendered as HTML, but the span is escaped
+    expect_equal(
+      htmlContainer,
+      '<div class="react-container">&lt;span id="test"&gt;Hello&lt;span style="font-weight: bold;"&gt; World&lt;/span&gt;&lt;/span&gt;</div>' #nolint
     )
   })
 })
